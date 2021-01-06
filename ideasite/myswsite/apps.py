104b70a9d0accf101d6c8f07cb4a2db5d7b21e11
InKyeongBae <inkkyung@gmail.com>
2021-01-07 05:10:26 +0900

    first commit

---
+++ ideasite/myswsite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyswsiteConfig(AppConfig):
+    name = 'myswsite'
---
+++ ideasite/myswsite/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class DevTool(models.Model):
+    name = models.CharField(max_length=50)
+    kind = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Idea(models.Model):
+    title = models.CharField(max_length=50, null=True)
+    image = models.ImageField(null=True, blank=True, upload_to="image")
+    content = models.TextField(null=True, blank=True)
+    interest = models.IntegerField(default=0)
+    devtool = models.ForeignKey(DevTool, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
